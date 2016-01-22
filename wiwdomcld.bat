4368ba18f6a70bd743dc44d6a0178aa700683285
Kommu <pl.scripiec@gmail.com>
2016-01-22 20:21:13 +0100

    Remove sleep function

--- wiwdomcld.bat
+++ wiwdomcld.bat
@@ -9,18 +9,18 @@ cd ..
 if not exist "output" mkdir output
 cd lib\
 
-@set /A timer=%timer%*30;
+@set /A timer=%timer%*60;
 
 :loop
 	set date_f=%date%
 	set date_f=%date_f:/=%
 	if not exist "..\output\%date_f%" mkdir ..\output\%date_f%
-
+		
 	set time_f=%time%
 	set time_f=%time_f::=%
 	set time_f=%time_f:.=_%
 	set time_f=%time_f:,=%
 	nircmd.exe savescreenshotfull ..\output\%date_f%\%time_f%.png
 	
-	SLEEP %timer%
+	timeout %timer% > NUL
 goto loop
